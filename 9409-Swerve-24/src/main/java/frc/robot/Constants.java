49d7ee1aeac30584a6150d1283c19b3dc59eaed4
cocowhynut <aeartkin@gmail.com>
2024-03-16 18:24:59 -0400

    tst

---
+++ 9409-Swerve-24/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+public class Constants {
+
+    public static final class JoystickConstants {
+
+    } 
+
+    public static final class SwerveModuleConstants {
+
+    }
+
+    public static final class DriveConstants {
+
+    }
+    
+}
